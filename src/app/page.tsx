3409cff4f9d379664a41cc353eba406d9a0978f0
imfaris <92919897+imfaris@users.noreply.github.com>
2025-07-31 17:47:26 +0300

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="font-sans h-screen flex flex-col items-center justify-center bg-[#16181d] p-0 overflow-hidden">
       <main className="flex flex-col items-center gap-12 w-full max-w-2xl group">
         <div className="flex flex-col items-center">
-          <Image
+          {/* <Image
             src="/logo_transparent.png"
             alt="ImFaris Logo"
             width={112}
@@ -13,7 +13,7 @@ export default function Home() {
             className="mb-2 rounded-full animate-float select-none bg-transparent"
             draggable={false}
             priority
-          />
+          /> */}
           <h1
             className="text-6xl sm:text-5xl font-extrabold text-transparent bg-clip-text mb-2 text-center tracking-tight select-none"
             style={{
@@ -24,7 +24,7 @@ export default function Home() {
               animation: 'gradientMove 5s linear infinite',
             }}
           >
-            ImFaris
+            imFaris
           </h1>
         </div>
         <style>{`
@@ -65,7 +65,7 @@ export default function Home() {
           className="mt-10 inline-flex items-center gap-2 text-emerald-300 hover:text-emerald-200 transition-colors text-base font-mono underline underline-offset-4"
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5"><path d="M12 2C6.477 2 2 6.484 2 12.021c0 4.428 2.865 8.184 6.839 9.504.5.092.682-.217.682-.483 0-.237-.009-.868-.014-1.703-2.782.605-3.369-1.342-3.369-1.342-.454-1.157-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.004.07 1.532 1.032 1.532 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.339-2.221-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.025A9.564 9.564 0 0 1 12 6.844c.85.004 1.705.115 2.504.337 1.909-1.295 2.748-1.025 2.748-1.025.546 1.378.202 2.397.1 2.65.64.7 1.028 1.595 1.028 2.688 0 3.847-2.337 4.695-4.566 4.944.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.744 0 .268.18.579.688.481C19.138 20.203 22 16.447 22 12.021 22 6.484 17.523 2 12 2Z" /></svg>
-          github.com/ImFaris
+          github.com/imFaris
         </a>
       </main>
     </div>
